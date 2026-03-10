276bdb7959b069307efd4b45420bd1430e63635d
Lakshan <lakshan.imantha02@gmail.com>
2026-03-10 17:34:06 +0530

    Implement custom UserRequest and UserResponse classes as DTOs.

---
+++ src/main/java/com/lakshan/task_flow/model/UserResponse.java
@@ -0,0 +1,38 @@
+package com.lakshan.task_flow.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse implements Serializable {
+
+    private String username;
+    private String email;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponse{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
